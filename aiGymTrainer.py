c203421e84e6f77134a975f37d4e3841efec562f
Zubair Atha <79466122+zubairatha@users.noreply.github.com>
2022-03-30 12:25:03 +0530

    Creating the menu and link with functions

--- aiGymTrainer.py
+++ aiGymTrainer.py
@@ -5,6 +5,90 @@ import numpy as np
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 
+def checkHandInBox(handX, handY, tlX, tlY, brX, brY):
+    if handX>tlX and handX<brX and handY>tlY and handY<brY:
+        return True
+    else:
+        return False
+
+def createMenu():
+    cv2.destroyAllWindows()
+
+    cap = cv2.VideoCapture(0)
+
+    curlTlx=220;curlBrx=400;curlTly=100;curlBry=160
+    deadliftTlx=220;deadliftBrx=400;deadliftTly=200;deadliftBry=260
+    squatTlx=220;squatBrx=400;squatTly=300;squatBry=360
+
+    with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            
+            # Recolor image to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image.flags.writeable = False
+        
+            # Make detection
+            results = pose.process(image)
+            # Recolor back to BGR
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+
+            try:
+                landmarks = results.pose_landmarks.landmark
+                
+                # Get coordinates
+
+                leftHand = [landmarks[mp_pose.PoseLandmark.LEFT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.LEFT_PINKY.value].y]
+                rightHand = [landmarks[mp_pose.PoseLandmark.RIGHT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_PINKY.value].y]
+
+                if(checkHandInBox(leftHand[0]*640,leftHand[1]*480 ,curlTlx, curlTly, curlBrx, curlBry) or checkHandInBox(rightHand[0]*640,rightHand[1]*480 ,curlTlx, curlTly, curlBrx, curlBry)):
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    curl()
+                    break
+
+                if(checkHandInBox(leftHand[0]*640,leftHand[1]*480 ,deadliftTlx, deadliftTly, deadliftBrx, deadliftBry) or checkHandInBox(rightHand[0]*640,rightHand[1]*480 ,deadliftTlx, deadliftTly, deadliftBrx, deadliftBry)):
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    deadlift()
+                    break
+
+                if(checkHandInBox(leftHand[0]*640,leftHand[1]*480 ,squatTlx, squatTly, squatBrx, squatBry) or checkHandInBox(rightHand[0]*640,rightHand[1]*480 ,squatTlx, squatTly, squatBrx, squatBry)):
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    squat()
+                    break
+                        
+            except:
+                pass
+
+            # curlTlx=220;curlBrx=400;curlTly=200;curlBry=260
+            cv2.rectangle(image, (curlTlx,curlTly), (curlBrx,curlBry), (245,117,16), -1)
+            cv2.putText(image, 'BICEP CURL', (220,160), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, cv2.LINE_AA)
+            # deadliftTlx=220;deadliftBrx=400;deadliftTly=300;deadliftBry=360
+            cv2.rectangle(image, (deadliftTlx,deadliftTly), (deadliftBrx,deadliftBry), (245,117,16), -1)
+            cv2.putText(image, 'DEADLIFT', (220,260), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, cv2.LINE_AA)
+            # squatTlx=220;squatBrx=400;squatTly=400;squatBry=460
+            cv2.rectangle(image, (squatTlx,squatTly), (squatBrx,squatBry), (245,117,16), -1)
+            cv2.putText(image, 'SQUAT', (220,360), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, cv2.LINE_AA)
+
+            mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                    mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                    mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                                    )  
+            cv2.imshow('Mediapipe Feed', image)
+            if cv2.waitKey(10) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 def calculate_angle(a,b,c):
     a = np.array(a) # First
     b = np.array(b) # Mid
@@ -20,6 +104,7 @@ def calculate_angle(a,b,c):
 
 
 def curl():
+    cv2.destroyAllWindows()
 
     cap = cv2.VideoCapture(0)
 
@@ -51,6 +136,15 @@ def curl():
                 shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
                 elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
                 wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+                leftHand = [landmarks[mp_pose.PoseLandmark.LEFT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.LEFT_PINKY.value].y]
+                rightHand = [landmarks[mp_pose.PoseLandmark.RIGHT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_PINKY.value].y]
+
+                if(checkHandInBox(leftHand[0]*640,leftHand[1]*480 ,590,20,670,80) or checkHandInBox(rightHand[0]*640,rightHand[1]*480,600,0,680,60)):
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    createMenu()
+                    break
                 
                 # Calculate angle
                 angle = calculate_angle(shoulder, elbow, wrist)
@@ -75,6 +169,9 @@ def curl():
             # Render curl counter
             # Setup status box
             cv2.rectangle(image, (0,0), (240,73), (245,117,16), -1)
+            cv2.rectangle(image, (590,20), (670,80), (245,117,16), -1)
+            cv2.putText(image, 'S', (590,79), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, cv2.LINE_AA)
             
             # Rep data
             cv2.putText(image, 'REPS', (15,12), 
@@ -82,6 +179,9 @@ def curl():
             cv2.putText(image, str(counter), 
                         (10,60), 
                         cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            # cv2.putText(image, 'CURL', 
+            #             (480,60), 
+            #             cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
             
             # Stage data
             cv2.putText(image, 'STAGE', (85,12), 
@@ -125,6 +225,7 @@ def checkDeadlift(a,b,c,d,checkUp=False):
 
 
 def deadlift():
+    cv2.destroyAllWindows()
 
     cap = cv2.VideoCapture(0)
 
@@ -152,6 +253,20 @@ def deadlift():
             try:
                 landmarks = results.pose_landmarks.landmark
 
+                # left_hand = landmarks[mp_pose.PoseLandmark.LEFT_INDEX.value]
+                # right_hand = landmarks[mp_pose.PoseLandmark.RIGHT_INDEX.value]
+                # left_knee = landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value]
+                # right_knee = landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value]
+
+                leftHand = [landmarks[mp_pose.PoseLandmark.LEFT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.LEFT_PINKY.value].y]
+                rightHand = [landmarks[mp_pose.PoseLandmark.RIGHT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_PINKY.value].y]
+
+                if(checkHandInBox(leftHand[0]*640,leftHand[1]*480 ,590,20,670,80) or checkHandInBox(rightHand[0]*640,rightHand[1]*480,600,0,680,60)):
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    createMenu()
+                    break
+
                 left_hand = landmarks[mp_pose.PoseLandmark.LEFT_INDEX.value]
                 right_hand = landmarks[mp_pose.PoseLandmark.RIGHT_INDEX.value]
                 left_knee = landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value]
@@ -169,6 +284,9 @@ def deadlift():
             
             # Setup status box
             cv2.rectangle(image, (0,0), (240,73), (245,117,16), -1)
+            cv2.rectangle(image, (590,20), (670,80), (245,117,16), -1)
+            cv2.putText(image, 'S', (590,79), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, cv2.LINE_AA)
             
             # Rep data
             cv2.putText(image, 'REPS', (15,12), 
@@ -176,6 +294,9 @@ def deadlift():
             cv2.putText(image, str(counter), 
                         (10,60), 
                         cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+            # cv2.putText(image, 'DEADLIFT', 
+            #             (460,60), 
+            #             cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
             
             # Stage data
             cv2.putText(image, 'STAGE', (85,12), 
@@ -222,7 +343,7 @@ def checkSquat(a,b,c,d,checkUp=False):
 
 
 def squat():
-
+    cv2.destroyAllWindows()
     cap = cv2.VideoCapture(0)
 
     # Deadlift counter variables
@@ -254,6 +375,15 @@ def squat():
                 left_knee = landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value]
                 right_knee = landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value]
 
+                leftHand = [landmarks[mp_pose.PoseLandmark.LEFT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.LEFT_PINKY.value].y]
+                rightHand = [landmarks[mp_pose.PoseLandmark.RIGHT_INDEX.value].x,landmarks[mp_pose.PoseLandmark.RIGHT_PINKY.value].y]
+
+                if(checkHandInBox(leftHand[0]*640,leftHand[1]*480 ,590,20,670,80) or checkHandInBox(rightHand[0]*640,rightHand[1]*480,600,0,680,60)):
+                    cap.release()
+                    cv2.destroyAllWindows()
+                    createMenu()
+                    break
+
                 if checkSquat(left_hip,right_hip,left_knee,right_knee):
                     stage = "down"
                 if stage=="down" and checkSquat(left_hip,right_hip,left_knee,right_knee,checkUp=True):
@@ -266,6 +396,9 @@ def squat():
             
             # Setup status box
             cv2.rectangle(image, (0,0), (240,73), (245,117,16), -1)
+            cv2.rectangle(image, (590,20), (670,80), (245,117,16), -1)
+            cv2.putText(image, 'S', (590,79), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, cv2.LINE_AA)
             
             # Rep data
             cv2.putText(image, 'REPS', (15,12), 
@@ -280,9 +413,7 @@ def squat():
             cv2.putText(image, stage, 
                         (80,60), 
                         cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
-            
-            
-            # Render detections
+
             mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                     mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
                                     mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
@@ -296,4 +427,6 @@ def squat():
         cap.release()
         cv2.destroyAllWindows()
 
-curl()
+createMenu()
+
+
